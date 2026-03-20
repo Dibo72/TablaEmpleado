7969d43d0a5231cd306c7244978b425bb0ea335f
Dibo72 <humanida133@gmail.com>
2026-03-20 08:55:12 +0100

    Ejercicio acabado

--- src/main/java/org/example/TablaEmpleado.java
+++ src/main/java/org/example/TablaEmpleado.java
@@ -1,17 +1,18 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class EjemploMaven {
+public class TablaEmpleado {
     public static void main(String[] args) {
         try(Connection conn = DriverManager.getConnection(
                 DBConfig.getUrl(),
                 DBConfig.getUser(),
-                DBConfig.getPassword())) {
-            System.out.println("Conexion establecida");
+                DBConfig.getPassword());
+            Statement stmt = conn.createStatement()) {
+            System.out.println("Conexion exitosa");
+
         }catch (Exception e){
             System.out.println(e);
         }
